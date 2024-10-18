61cec7dece3ca68f51136ebf81d20deb3067f8bf
sergeyiksanov <sergeyiksanov25@gmail.com>
2024-10-18 15:22:57 +0300

    add token entity

---
+++ internal/entity/token.go
@@ -0,0 +1,16 @@
+package entity
+
+import "github.com/jackc/pgx/v5/pgtype"
+
+type Token struct {
+	JTI       string           `gorm:"column_id:jti,primaryKey"`
+	SubjectId int64            `gorm:"column_id:subject_id"`
+	TokenType string           `gorm:"column_id:token_type"`
+	Revoked   bool             `gorm:"column_id:revoked"`
+	IssuedAt  pgtype.Timestamp `gorm:"column_id:issued_at"`
+	ExpiresAt pgtype.Timestamp `gorm:"column:expires_at"`
+}
+
+func (c *Token) TableName() string {
+	return "issued_jwt_token"
+}
